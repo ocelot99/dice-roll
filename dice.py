f537558ca97e764dbd34510766d91b1954ab6ec4
ocelot99 <30462615+ocelot99@users.noreply.github.com>
2017-08-24 07:31:20 +0100

    Comments
    Added comments to the code

--- dice.py
+++ dice.py
@@ -1,40 +1,42 @@
+#made by ocelot
+#23/08
 import random
 import math
 
-def main():
+def main():#I/O
     try:
-        dice = int(input("Enter number of dice: "))
-        if dice == 0:
+        dice = int(input("Enter number of dice: "))#Gets number of dice.
+        if dice == 0:#If 0 then loop
             print("Please enter a number higher than 0:")
             main()
-        sides = int(input("Enter sides on the dice: "))
+        sides = int(input("Enter sides on the dice: "))#Gets number of sides for dice.
         if sides == 0:
             print("Please enter a number higher than 0:")
             main()
-        notrandom(dice, sides)
+        notrandom(dice, sides)#Passes variables to the dice roll function.
     except (ValueError):
         print("Invalid Input")
         main()
         
-def notrandom(dice : int, sides : int):
-    total = 0
+def notrandom(dice : int, sides : int):#Rolls the dice.
+    total = 0#Initializing some variables.
     maxroll = (sides * dice)
     lowest = math.inf
     highest = -math.inf
     average = 0
-    for x in range(0,dice):
+    for x in range(0,dice):#Loops the dice roll code for each dice.
         number = random.randint(1,sides)
-        if float(lowest) > int(number):
+        if float(lowest) > int(number):#This if statement calculates the lowest roll.
             lowest = number
-        if float(highest) < int(number): 
+        if float(highest) < int(number):#This statement calculates the highest roll.
             highest = number
-        print(x + 1 , ":" , number)
+        print(x + 1 , ":" , number)#Displays the result of each roll.
         total = total + number
-    average = (total / dice)
-    print ("sum of all rolls: ", total, "/", maxroll)
-    print ("average roll: ", average)
-    print ("lowest roll: ", lowest)
+    average = (total / dice)#Calculates average roll.
+    print ("Sum of all rolls: ", total, "/", maxroll)#This section prints out the roll and roll data.
+    print ("Average roll: ", average)
+    print ("Lowest roll: ", lowest)
     print ("Highest roll: ", highest)
-    main()
+    main()#Loops back to the start
 
 main()
