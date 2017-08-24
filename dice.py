123430bf1e81c8740799a0077397d1bd0d48c593
ocelot99 <30462615+ocelot99@users.noreply.github.com>
2017-08-24 07:15:42 +0100

    exception handling and statistics
    added exception handling to the dice and sides integers
    also added some roll stats such as highest roll, lowest roll, and average

--- dice.py
+++ dice.py
@@ -1,18 +1,34 @@
 import random
+import math
 
 def main():
-    dice = int(input("Enter number of dice: "))
-    sides = int(input("Enter sides on the dice: "))
-    notrandom(dice, sides)
-    
+    try:
+        dice = int(input("Enter number of dice: "))
+        sides = int(input("Enter sides on the dice: "))
+        notrandom(dice, sides)
+    except (ValueError):
+        print("Invalid Input")
+        main()
+        
 def notrandom(dice : int, sides : int):
     total = 0
     maxroll = (sides * dice)
+    lowest = math.inf
+    highest = -math.inf
+    average = 0
     for x in range(0,dice):
         number = random.randint(1,sides)
+        if float(lowest) > int(number):
+            lowest = number
+        if float(highest) < int(number): 
+            highest = number
         print(x + 1 , ":" , number)
         total = total + number
+    average = (total / dice)
     print ("sum of all rolls: ", total, "/", maxroll)
+    print ("average roll: ", average)
+    print ("lowest roll: ", lowest)
+    print ("Highest roll: ", highest)
     main()
 
 main()
